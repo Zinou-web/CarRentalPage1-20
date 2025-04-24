acf971758278934176611ae91c1d7aeddaf3f0ec
mac <mac@MacBook-Air-de-mac.local>
2025-04-24 12:04:34 +0100

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.myapplication.ui.screens.ThirdScreen.ThirdScreen
+import com.example.myapplication.ui.screens.second.SecondScreen
+import com.example.myapplication.ui.screens.welcome.WelcomeScreen
+import com.example.myapplication.ui.theme.MyApplicationTheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            MyApplicationTheme {
+
+                    WelcomeScreen()
+                    SecondScreen()
+                    ThirdScreen()
+
+
+            }
+        }
+    }
+}
+
+
---
+++ app/src/main/java/com/example/myapplication/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+
+val PrimaryGreen = Color(0xFF4E6A41)
+val SecondaryGreen = Color(0xFF149459)
+val TextDark = Color(0xFF222629)
+val White = Color.White
+val Page2Green = Color(0xFF149459)
+val BackgroundWhite = Color(0xFFF2F5FA)
+val TextGray = Color(0xFF8E8E93)
+
